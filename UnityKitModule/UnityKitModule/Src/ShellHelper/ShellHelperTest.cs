880da90136d7b0f73f34f98770808bd4f654453f
liangddyy <liangddyy@gmail.com>
2019-11-05 16:38:41 +0800

    fix

--- UnityKitModule/UnityKitModule/Src/ShellHelper/ShellHelperTest.cs
+++ UnityKitModule/UnityKitModule/Src/ShellHelper/ShellHelperTest.cs
@@ -8,23 +8,23 @@ using UnityEditor;
 /// </summary>
 public class ShellHelperTest
 {
-    [MenuItem("ShellHelperTest/ls")]
-    public static void ls()
-    {
-#if UNITY_EDITOR_OSX
-        ShellHelper.ShellRequest req = ShellHelper.ProcessCommand("ls", "");
-        req.onLog += delegate(int arg1, string arg2) { Debug.Log(arg2); };
-
-#endif
-    }
-
-    [MenuItem("ShellHelperTest/dir")]
-    public static void dir()
-    {
-		ShellHelper.ShellRequest req = ShellHelper.ProcessCommand( "dir","");
-		req.onLog += delegate(int arg1, string arg2) {
-			Debug.Log(arg2);
-		};
-
-    }
+//    [MenuItem("ShellHelperTest/ls")]
+//    public static void ls()
+//    {
+//#if UNITY_EDITOR_OSX
+//        ShellHelper.ShellRequest req = ShellHelper.ProcessCommand("ls", "");
+//        req.onLog += delegate(int arg1, string arg2) { Debug.Log(arg2); };
+//
+//#endif
+//    }
+//
+//    [MenuItem("ShellHelperTest/dir")]
+//    public static void dir()
+//    {
+//		ShellHelper.ShellRequest req = ShellHelper.ProcessCommand( "dir","");
+//		req.onLog += delegate(int arg1, string arg2) {
+//			Debug.Log(arg2);
+//		};
+//
+//    }
 }
